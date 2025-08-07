9dd22dfc9d5588c74d3b4acefef71cf9e01c15ec
Andrew Souza <souzaphonix@pm.me>
2025-08-07 04:05:21 -0700

    Add functionality to execute single shell commands.

--- src/main.c
+++ src/main.c
@@ -3,26 +3,62 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include "../include/queue.h"
 
 #define MAX_INPUT 1024
+#define MAX_WORD_LEN 50
 
-int main(int argc, const char** argv) {
+int main(void) {
   char input[MAX_INPUT];
-  while (strcmp(input, "exit") != 0) {
-    char* words[MAX_INPUT];
-    char temp;
-    size_t i = 0;
+  while (1) {
     printf("dssh> ");
-    while (i < MAX_INPUT - 1 && scanf("%c", &temp) == 1 && temp != '\n') {
-      input[i++] = temp;
-    }
-    input[i] = '\0';
+    if (!fgets(input, MAX_INPUT, stdin))
+      break;
 
-    i = 0;
-    while (input[i] != '\0') {
-      printf("%c", input[i++]);
+    input[strcspn(input, "\n")] = '\0'; // remove newline
+    if (input[0] == '\0')
+      continue;
+    if (strcmp(input, "exit") == 0)
+      break;
+
+    WordQueue queue;
+    initWordQueue(&queue);
+
+    char *token = strtok(input, " ");
+    while (token != NULL) {
+      Word *queuedWord = malloc(sizeof(Word));
+      if (!queuedWord) {
+        fprintf(stderr, "Memory allocation failed");
+        exit(EXIT_FAILURE);
+      }
+      initWord(queuedWord, token);
+      enqueue(&queue, queuedWord);
+      token = strtok(NULL, " ");
     }
-    printf("\n");
+    char command[MAX_WORD_LEN];
+    strcpy(command, queue.head->word);
+
+    int rc = fork();
+
+    if (rc == 0) {
+      char *args[queue.size + 1];
+      Word *tmp = queue.head->next;
+      args[0] = command;
+      for (int i = 0; i < queue.size; i++) {
+        args[1] = tmp->word;
+        tmp = tmp->next;
+      }
+      args[queue.size] = NULL;
+      execvp(command, args);
+      perror("exec failure");
+      exit(EXIT_FAILURE);
+    } else {
+      wait(NULL);
+    }
+
+    freeWordQueue(&queue);
   }
 
   return 0;
